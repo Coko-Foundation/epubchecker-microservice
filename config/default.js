d0deb483f0059d55466e6ddca5443c23e069b38c
Yannis Barlas <yannisbarlas@gmail.com>
2024-12-13 13:14:21 +0200

    fix(*): run create client from env script on startup

--- config/default.js
+++ config/default.js
@@ -4,4 +4,18 @@ module.exports = {
   components,
   subscriptionsDb: {},
   useGraphQLServer: false,
+
+  onStartup: [
+    {
+      label: 'Create client runner through environment',
+      execute: async () => {
+        const {
+          createServiceClientFromEnvironment,
+          /* eslint-disable-next-line global-require */
+        } = require('@coko/service-auth')
+
+        await createServiceClientFromEnvironment()
+      },
+    },
+  ],
 }
